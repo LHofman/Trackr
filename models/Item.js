d44ce1552a0e206529bbff5de0dfc9313b5277df
Lennert Hofman <hofmanlennert@gmail.com>
2018-09-02 13:50:17 +0200

    items CRUD

---
+++ models/Item.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+export default mongoose.model(
+  'Item',
+  mongoose.Schema({
+    type: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    title_id: {
+      type: String,
+      required: true
+    },
+    releaseDate: {
+      type: String,
+      required: true
+    },
+    author: String,
+    ongoing: Boolean
+  })
+);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,62 @@
 import express from 'express';
 
+import Item from '../models/Item';
+
 const router = express.Router();
+
+const STATUS_500_MESSAGE = 'Something went wrong';
+
+//#region items
+
+router.get('/items', (req, res, next) => {
+  Item.find((err, items) => {
+      if (err) return res.status(500).send(STATUS_500_MESSAGE);
+      res.json(items);
+    });
+});
+
+router.get('/items/:id', (req, res, next) => {
+  Item.findById(req.params.id, (err, item) => {
+      if (err) return res.status(404).send('Item not found');
+      res.json(item);
+    });
+});
+
+router.post('/items', (req, res, next) => {
+  const item = new Item(req.body);
+  item.save((err, item) => {
+    if (err) return res.status(500).send('Something went wrong');
+    res.json(item);
+  });
+});
+
+router.put('/items/:id', (req, res, next) => {
+  const item = req.body;
+  Item.findOneAndUpdate(
+    { _id: req.params.id },
+    item,
+    {new: true}, //options
+    (err, newItem) => {
+      if (err) return res.status(404).send('Item not found');
+      res.json(item);
+    }
+  );
+});
+
+router.delete('/items/:id', auth, (req, res, next) => {
+  Item.findById(req.params.id, (err, item) => {
+    if (err) return res.status(500).send({success: false, msg: 'Item not found'});
+    
+    item.remove((err, item) => {
+      if (err) return res.status(500).send('Something went wrong');
+      res.json({
+        success: true,
+        msg: `${item.title} has successfully been removed.`
+      });
+    });
+  });
+});
+
+//#endregion items
 
 export default router;
