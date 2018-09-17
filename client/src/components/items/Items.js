cb41f4bd0d3ddb95e527329291bbde45e7b10b36
Lennert Hofman <hofmanlennert@gmail.com>
2018-09-17 12:44:48 +0200

    filter items on title and type

--- client/src/components/items/Items.js
+++ client/src/components/items/Items.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, List } from 'semantic-ui-react';
+import { Button, Dropdown, Input, List } from 'semantic-ui-react';
 
 import Item from './Item';
 
 import fetch from '../../utils/fetch';
 import isLoggedIn from '../../utils/isLoggedIn';
+import typeOptions from './typeOptions';
 
 export default class Items extends Component {
   constructor() {
     super();
     this.state = {
-      items: []
+      items: [],
+      typeFilter: '',
+      titleFilter: ''
     }
   }
 
@@ -27,14 +30,38 @@ export default class Items extends Component {
     }).catch(console.log);
   }
 
+  onTitleFilterChange(event) {
+    if (event.key === 'Enter') this.onFilterChange(event);
+  }
+
+  onFilterChange(event) {
+    this.handleValueChange(event.target.name, event.target.value);
+  }
+
+  handleValueChange(name, value) {
+    this.setState({ [name]: value });
+  }
+
   render() {
-    const items = this.state.items.map(item => 
+    const {typeFilter, titleFilter} = this.state;
+    const filteredItems = this.state.items.filter(item => 
+      item.title.toString().toLowerCase().indexOf(
+        titleFilter.toString().toLowerCase()
+      ) !== -1 &&
+      (typeFilter === '' || item.type === typeFilter)
+    );
+    
+    const items = filteredItems.map(item => 
       <Item key={item._id} item={item}></Item>
     );
 
     return (
       <div>
         <h2>Items</h2>
+        <Input name='titleFilter' onKeyPress={this.onTitleFilterChange.bind(this)} icon='search' placeholder='Search...' />&nbsp;&nbsp;&nbsp;
+        <Dropdown placeholder='Type' name='typeFilter' selection value={''}
+          options={[{ text: '---No Filter---', value: '' }, ...typeOptions]}
+          onChange={(param, data) => this.handleValueChange('typeFilter', data.value)} />&nbsp;&nbsp;&nbsp;
         {
           isLoggedIn() && 
           <Button positive circular floated='right' icon='plus' as={Link} to='/items/add' />
